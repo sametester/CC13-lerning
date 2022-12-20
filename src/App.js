d8004b898eac45a089d6aae13e0dfc809b69a705
sametester <sittichok.boo@gmail.com>
2022-12-20 16:19:06 +0700

    finish lab jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+// Lab 1
+// function App() {
+//   const fullName = 'Same';
+//   const birthYear = 2000;
 
+//   return (
+//     <div>
+//       <h1>Full NAme: {fullName}</h1>
+//       <p>Age: {2022 - birthYear}</p>
+//     </div>
+//   );
+// }
+
+// Lab 2
+// function App() {
+//   const calcBMI = (h, w) => w / h ** 2;
+//   const height = 1.7;
+//   const weight = 140;
+
+//   // const BMI = 140 / (1.7 * 1.7);
+
+//   return (
+//     <div className="App">
+//       {/* <h1>BMI ={BMI}</h1> */}
+//       <h1>{calcBMI(height, weight)}</h1>;
+//     </div>
+//   );
+// }
+
+// Lab 3
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label htmlFor="username">Username</label>
+      <input className="username" id="username" name="username" />
     </div>
   );
 }
-
 export default App;
